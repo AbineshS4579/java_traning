2fd6d606e4f6c7d4824e056bf27cb1a4f752c2a1
Abinesh S <abinesh781845@gmail.com>
2026-07-18 14:30:30 +0530

    jagged

---
+++ String/jagged.java
@@ -0,0 +1,12 @@
+public class jagged {
+    public static void main(String[] args) {
+     int a[][]={{10,20,30,40},{34,5,65},{45,98,79,56,}};
+     for(int[] b:a){
+     for(int c:b){
+        System.out.print(c+" ");
+     }
+     System.out.println();
+     }   
+    }
+    
+}
