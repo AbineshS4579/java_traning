a076fd36e342f9800046611603a9c2bbc8555070
Abinesh S <abinesh781845@gmail.com>
2026-07-26 23:45:58 +0530

    ATM machine

--- real_world_problems/ATM.java
+++ real_world_problems/ATM.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
 class bank{
-    private int pin =1207,balance;
-    bank(int p)throws Exception{
-        if(pin!=p){
-            throw new Exception("invalid pin");
-        }
+    private int pin,balance;
+    bank(int pin,int balance){
+        this.pin=pin;
+        this.balance=balance;
     }
-    void am(int i,int d){
-        balance=i;
+    int banks(){
+       return pin;
+    }
+    void am(int d){
         balance+=d;
     }
     void display(int w)throws Exception{
         if(balance<w)
-            throw new Exception("invalid balance");
+            throw new Exception("Invalid balance.....");
         balance-=w;
     }
     int show(){
@@ -23,13 +24,60 @@ class bank{
         public static void main(String[] args){
             Scanner s=new Scanner(System.in);
             try{
-                int pi=s.nextInt();
-                bank o=new bank(pi);
-                int n=s.nextInt(),m=s.nextInt();
-                o.am(n,m);
-                int w=s.nextInt();
-                o.display(w);
-                System.out.println(o.show());
+                System.out.print("Set a ATM pin:");
+                int pt=s.nextInt();
+                System.out.print("Enter the initial depositing amount:");
+                int ib=s.nextInt();
+
+                bank o = new bank(pt,ib);
+                boolean l=true;
+                int d=2,y=0;
+                do {
+
+                while(l) {
+                    System.out.print("Enter ATM pin:");
+                    int p = s.nextInt();
+                    if(o.banks()==p){
+                        System.out.println("pin is correct...");
+                        d=2;
+                        break;
+                    }
+                    else if(d>0){
+                        System.out.println("please enter vailed pin you have only "+d+" chance");
+                        d--;
+                    }
+                    else {
+                        System.out.println("your account is locked due invalid pin entered.....");
+                        return ;
+                    }
+                }
+
+                    System.out.print("1.Deposite\n2.Withdraw\n3.check Balance\nOther wise Enter :0 \nEnter the what the process you need:");
+                    int x = s.nextInt();
+                    y++;
+                    switch (x) {
+                        case 1:
+                            System.out.print("Enter Deposit amount:");
+                            int m = s.nextInt();
+                            o.am(m);
+                            System.out.println("Balance:" + o.show());
+                            break;
+                        case 2:
+                            System.out.print("Enter Withdraw amount:");
+                            int w = s.nextInt();
+                            o.display(w);
+                            System.out.println("Balance:" + o.show());
+                            break;
+                        case 3:
+                            System.out.println("Balance:" + o.show());
+                            break;
+                        default:
+                            return;
+
+                    }
+                }while(y<5);
+                System.out.println("Your limit is over....");
+
             }catch(Exception e){
                 System.out.println(e.getMessage());
             }
