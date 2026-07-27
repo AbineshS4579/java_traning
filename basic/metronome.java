46ce25c57e10ab9da6c89fcc57c62ac3ed12a71f
Abinesh S <abinesh781845@gmail.com>
2026-07-27 23:20:46 +0530

    ss

---
+++ basic/metronome.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class metronome{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        double n=s.nextDouble();
+        double x=n/4;
+        System.out.println(x);
+    }
+}
